17e32a2180303de25b9a5e7591232444653ab5fb
adam-gill <110919227+adam-gill@users.noreply.github.com>
2023-11-10 10:03:46 -0500

    attempted menu

---
+++ index.js
@@ -0,0 +1,9 @@
+function closeMenu() {
+    const menu = document.getElementById("menu");
+    menu.style.visibility = 'none';
+}
+
+function openMenu() {
+    const menu = document.getElementById("menu");
+    menu.style.visibility = 'visible';
+}
